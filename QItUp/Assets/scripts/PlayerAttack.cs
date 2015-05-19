f7d181db4e87d2090d3284447feab4889ad0540b
Jeffrey Johnson <jefjohms@gmail.com>
2015-05-19 11:10:40 -0700

    updated key

---
+++ QItUp/Assets/scripts/PlayerAttack.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public float strength = 10f;
+    public LayerMask canHit;
+    public Transform attackPoint;
+    public GameObject attackPrefab;
+
+    private float timer = 0f;
+    private Animator anim;
+    private bool launching = false;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+    }
+
+    public void StartAttack()
+    {
+        if(timer <= 0 && !launching && attackPrefab != null)
+        {
+            anim.SetTrigger("Attack");
+            launching = true;
+        }
+    }
+
+    public void FireAttack()
+    {
+        GameObject atk = Instantiate(attackPrefab, attackPoint.position, attackPoint.rotation) as GameObject;
+        timer = atk.GetComponentInChildren<Striker>().cooldown;
+        Physics2D.IgnoreCollision(atk.GetComponentInChildren<Collider2D>(), GetComponent<Collider2D>());
+        launching = false;
+    }
+
+    public void Attack()
+    {
+        Collider2D other = Physics2D.OverlapCircle(attackPoint.position, .2f, canHit);
+        if (other != null)
+        {
+            other.GetComponent<MeleeMonster>().TakeDamage(strength);
+        }
+    }
+}
