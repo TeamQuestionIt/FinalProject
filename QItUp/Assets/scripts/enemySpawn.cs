63e79c13488382b7c3235cac27d6ab58d0de5a9f
JeffreyMJohnson <jefjohms@gmail.com>
2015-05-24 21:59:06 -0700

    have a crude spawn hooked up.

---
+++ QItUp/Assets/scripts/enemySpawn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySpawn : MonoBehaviour {
+    public GameObject enemyPrefab;
+
+    public float maxTimeBetween;
+
+    private float timer = 0;
+    private float currentTimeBetween = 0;
+	void Start () {
+        currentTimeBetween = maxTimeBetween;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer < currentTimeBetween)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Spawn();
+            currentTimeBetween = Random.Range(0, maxTimeBetween);
+            timer = 0;
+        }
+	}
+
+    public void Spawn()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+}
