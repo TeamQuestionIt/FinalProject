4eb49d9f8946db0157f5a88fe51f6995852e3536
BryceX <btau20102@yahoo.com>
2015-05-26 12:08:14 -0700

    commitidk

---
+++ QItUp/Assets/scripts/MonsterHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterHealth : MonoBehaviour {
+
+	public GameObject healthBar;
+	public GameObject monster;
+	public Transform monsterPos;
+	
+	public float xOffset;
+	public float yOffset;
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (healthBar != null && monsterPos != null) 
+		{
+			//healthPos = monsterPos.position;
+			//healthPos.x += xOffset;
+			//healthPos.y += yOffset;
+			transform.position = Camera.main.WorldToScreenPoint (monsterPos.transform.position);
+			//healthBar.GetComponent<RectTransform> ().sizeDelta = new Vector2 (monster.GetComponent<HealthTracker> ().maxHealth, 20f);
+		}
+
+		if (monsterPos == null)
+			Destroy (healthBar);
+
+
+	}
+}
