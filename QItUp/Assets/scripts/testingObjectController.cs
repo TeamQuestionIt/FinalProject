715a9252ca7a136c9f18431b2b5994c79ba58f35
QuinnJMosher <Darkstaro59@gmail.com>
2015-05-26 11:57:49 -0700

    adding prexisting scripts

---
+++ QItUp/Assets/scripts/testingObjectController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testingObjectController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0.11f, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -0.11f, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.11f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.11f, 0, 0);
+        }
+	}
+}
