d21f6ecbe40518e75ccedaf2b5a6e00910a3361e
BryceX <btau20102@yahoo.com>
2015-05-27 10:24:46 -0700

    random commit

---
+++ QItUp/Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+   
+    public Image healthBar;
+    private float filled;
+    private float maxHP;
+    private Player playerScript;
+
+	// Use this for initialization
+	void Start () 
+    {
+        playerScript = GetComponent<Player>();
+        maxHP = playerScript.hitPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void OnGUI() 
+    {
+    
+    }
+}
