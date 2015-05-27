5761ed5b8c2d35b94f267a278504c110323ebb91
QuinnJMosher <Darkstaro59@gmail.com>
2015-05-27 10:45:14 -0700

    Added assets for menus started main menu

---
+++ QItUp/Assets/ArrowScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour {
+
+    public enum currentMenu
+    {
+        menu_Main,
+        menu_Control,
+        menu_Pause
+    }
+
+
+    public currentMenu startingMenu = currentMenu.menu_Main;
+    public int startingPosition = 0;
+    GameObject positionContainer;
+
+    //menu length trackers hardcoded for safty and time
+    const int mainMenuItemCount = 4;
+    const int controlMenuItemCount = 1;
+    const int pauseMenuItemCount = 0;//changeLater
+
+	// Use this for initialization
+	void Start () {
+        positionContainer = GameObject.Find("ArrowPositions");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ QItUp/Assets/MainMenuBGPan.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuBGPan : MonoBehaviour {
+
+    public float startY = 6.5f;
+    public float endY = -6.5f;
+
+    public float speed = 0.11f;
+    bool movingDown = true;
+    public float stopTime = 5.0f;
+    float stopingTime = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (stopingTime > 0)
+        {
+            stopingTime -= Time.deltaTime;
+        } 
+        else if (movingDown)
+        {
+            transform.Translate(0, -speed, 0);
+            if (transform.position.y <= endY)
+            {
+                movingDown = false;
+                stopingTime = stopTime;
+            }
+        }
+        else
+        {
+            transform.Translate(0, speed, 0);
+            if (transform.position.y >= startY)
+            {
+                movingDown = true;
+                stopingTime = stopTime;
+            }
+        }
+	}
+}
