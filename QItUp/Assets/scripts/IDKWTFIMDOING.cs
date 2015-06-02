9c807fd59edd906aaa99be5570c809feaf5b8992
BryceX <btau20102@yahoo.com>
2015-06-02 14:37:20 -0700

    UI shiznizzle

---
+++ QItUp/Assets/scripts/IDKWTFIMDOING.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class IDKWTFIMDOING : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+        GetComponent<Text>().text = "Score: " + ScoreManager.Score;
+
+	}
+}
