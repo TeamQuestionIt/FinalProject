3225820cc3d615a3c67bc63d63fc17ee7489a02b
BryceX <btau20102@yahoo.com>
2015-06-02 10:29:29 -0700

    refactor and level end

---
+++ QItUp/Assets/scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+    
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "Player")
+        {
+            Application.LoadLevel("LevelTwo");
+            Debug.Log("you ended the level");
+        }
+
+    }
+}
