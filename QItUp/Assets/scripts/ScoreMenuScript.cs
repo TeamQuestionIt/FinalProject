7748b054d65ab77c835a0d0762834c8c77e825a7
QuinnJMosher <Darkstaro59@gmail.com>
2015-06-01 17:28:44 -0700

    hooked highscores menu up

---
+++ QItUp/Assets/scripts/ScoreMenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreMenuScript : MonoBehaviour {
+
+    public GameObject ScoreText0;
+    public GameObject ScoreText1;
+    public GameObject ScoreText2;
+
+	// Use this for initialization
+	void Start () {
+        ScoreManager scoreManager = gameObject.GetComponent<ScoreManager>();
+
+        scoreManager.LoadHighscores();
+
+        List<int> highscores = scoreManager.GetHighScores();
+
+        ScoreText0.GetComponent<TextMesh>().text = "0";
+        ScoreText1.GetComponent<TextMesh>().text = "0";
+        ScoreText1.GetComponent<TextMesh>().text = "0";
+
+        if (highscores.Count >= 1)
+        {
+            ScoreText0.GetComponent<TextMesh>().text = highscores[0].ToString();
+
+            if (highscores.Count >= 2)
+            {
+                ScoreText1.GetComponent<TextMesh>().text = highscores[1].ToString();
+
+                if (highscores.Count >= 3)
+                {
+                    ScoreText2.GetComponent<TextMesh>().text = highscores[2].ToString();
+                }
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
