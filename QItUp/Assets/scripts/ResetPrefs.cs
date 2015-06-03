f1c47d3e0d3a6d0cb11425c8e40f6dc9bd7d9331
Jeffrey Johnson <jefjohms@gmail.com>
2015-06-03 09:48:39 -0700

    working on high score

---
+++ QItUp/Assets/scripts/ResetPrefs.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPrefs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.DeleteAll();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
